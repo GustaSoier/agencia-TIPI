db7742d7f52afb7095a3d07a85805670b00c9d90
GustaSoier <gsampaiosoier0@gmail.com>
2023-06-07 00:41:51 -0300

    Agência-TIPI
    Projeto em andamento, ja foi feito HTML, CSS e JS (em andamento).

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$('.banner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+});
+
+
+$('.slideDepo').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+});
+
+
+$('.galeria').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+
