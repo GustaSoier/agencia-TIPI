a6c408cf386abb3084736e7c833197d5e478beea
GustaSoier <gsampaiosoier0@gmail.com>
2023-06-14 19:54:05 -0300

    Agência TIPI
    Adicionei alguns carrosséis e estou deixando responsivo.

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ $('.banner').slick({
     dots: true,
 });
 
-
 $('.slideDepo').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
@@ -15,12 +14,44 @@ $('.slideDepo').slick({
   dots: true,
 });
 
-
 $('.galeria').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
+
+responsive: [
+{
+  breakpoint: 1024,
+  settings: {
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  infinite: true,
+  }
+},
+{
+  breakpoint: 600,
+  settings: {
+    slidesToShow: 2,
+    slidesToScroll: 2
+}
+},
+{
+  breakpoint: 480,
+  settings: {
+  slidesToShow: 1,
+  slidesToScroll: 1
+}
+}
+]
 });
 
 
+// MENU MOBILE
+document.querySelector(".abrir-menu").onclick = function () {
+  document.documentElement.classList.add("menu-ativo");
+}
+
+document.querySelector(".fechar-menu").onclick = function () {
+  document.documentElement.classList.remove("menu-ativo")
+}
